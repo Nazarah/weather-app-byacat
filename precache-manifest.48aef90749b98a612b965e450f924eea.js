f6c0e531f81bdf19ca9e86436a29b4bbc8dd7119
Nazarah <nhasan1987@gmail.com>
2019-03-06 22:27:52 +0200

    Updates

---
+++ precache-manifest.48aef90749b98a612b965e450f924eea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "78a84f7d7157e808e5bcf8935087c860",
+    "url": "/weather-app-byacat/static/media/WeatherAppBG.78a84f7d.png"
+  },
+  {
+    "revision": "72e373fb5bb5904f733a",
+    "url": "/weather-app-byacat/static/js/runtime~main.72e373fb.js"
+  },
+  {
+    "revision": "070463cb519eb0dc2c8e",
+    "url": "/weather-app-byacat/static/js/main.070463cb.chunk.js"
+  },
+  {
+    "revision": "a496026b029553e40ac9",
+    "url": "/weather-app-byacat/static/js/2.a496026b.chunk.js"
+  },
+  {
+    "revision": "070463cb519eb0dc2c8e",
+    "url": "/weather-app-byacat/static/css/main.ae5277c3.chunk.css"
+  },
+  {
+    "revision": "a496026b029553e40ac9",
+    "url": "/weather-app-byacat/static/css/2.1d9ca6a4.chunk.css"
+  },
+  {
+    "revision": "15b25dd37015b332b2b377a2d2cd1bb7",
+    "url": "/weather-app-byacat/index.html"
+  }
+];
